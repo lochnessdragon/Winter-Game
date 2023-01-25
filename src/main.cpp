73d606a246d1d012feb3f4d4ec946d389ffaf25e
Lochnessdragon <39635811+lochnessdragon@users.noreply.github.com>
2023-01-25 08:02:44 -0600

    More stuff:

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <filesystem>
 
 #include <log.h>
 #include <window.h>
@@ -8,10 +9,10 @@
 #include <camera.h>
 
 float vertices[] = {
-    0.0f, 0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
     1.0f, 1.0f, 0.0f,
-    0.0f, 1.0f, 0.0f
+    -1.0f, 1.0f, 0.0f
 };
 
 int indices[] {
@@ -22,6 +23,7 @@ int indices[] {
 int main() {
     try {
         Log("Opening Window!");
+        std::cout << "CWD: " << std::filesystem::current_path() << std::endl;
 
         Window win("Minecraft - v 0.1", 600, 400);
 
@@ -35,18 +37,18 @@ int main() {
         commonShader.use();
 
         glm::ivec2 winSize = win.getWindowSize();
-        Camera camera(&win, glm::vec3(4.0f, 0.0f, 10.0f), glm::vec3(0.0f), winSize.x / winSize.y, glm::radians(120.0f), 0.1f, 1000.0f);
+        Camera camera(&win, glm::vec3(4.0f, 0.0f, 10.0f), glm::vec3(0.0f), (float) winSize.x / (float) winSize.y, glm::radians(120.0f), 0.1f, 1000.0f);
 
         glClearColor(0.1f, 0.7f, 0.7f, 1.0f);
 
-        float deltaTime = 0.0f;
-        float lastTime = glfwGetTime();
+        double deltaTime = 0.0f;
+        double lastTime = glfwGetTime();
 
         while (!win.shouldClose()) {
 
             // tick
             {
-                float now = glfwGetTime();
+                double now = glfwGetTime();
                 deltaTime = now - lastTime;
                 lastTime = now;
             }
